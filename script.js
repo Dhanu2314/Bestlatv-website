74a2f23b7c3b76f112cfcccb1e6b1f3239791fcb
Dhanashri Patil <140504460+Dhanu2314@users.noreply.github.com>
2025-03-16 15:27:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Loaded Successfully!");
+});
+
+function showMore() {
+    alert("Bestla Industries is growing fast! Stay tuned for more updates.");
+}
+
+function contactUs() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
